e98d8956a9e0e89016a3cbf528f3ba602b1b198f
ppantazos <panos@schoox.com>
2020-06-04 19:18:53 +0300

    check

--- Groups/environment.py
+++ Groups/environment.py
@@ -1,8 +1,8 @@
 from config import set_before_feature, set_after
 
 def before_feature(context, feature):
-        set_before_feature(context, 617024721)
-        context.group_id = 63291
+    set_before_feature(context, 617024721)
+    context.group_id = 63291
 
 
 def after_feature(context, feature):
